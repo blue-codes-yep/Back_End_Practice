6cc0b282cb5c1dfc45a52231d30b0ead870440dd
Blue Lindsey <linzblue5@gmail.com>
2020-02-27 17:26:35 -0500

    First Commits of working version

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    app = express(),
+    es6Renderer = require('express-es6-template-engine'),
+    rootController = require('./routes/restaruants');
+
+app.engine('html', es6Renderer);
+app.set('views','views');
+app.set('view engine', 'html');
+
+app.listen(9000, () => {
+    console.log('Power level over 9000');
+});
+
+app.use('/', rootController);
